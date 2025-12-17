faf43b7027a9f202edede3f6b4190a7a947470c7
Tristan Hodgson <hello@tristanhodgson.com>
2025-12-17 16:33:04 +0000

    Parallelise matrix multiplication

--- 3multiply.sql
+++ 3multiply.sql
@@ -1,26 +1,4 @@
-DROP FUNCTION IF EXISTS elm_mul(text, text, int, int);
 DROP FUNCTION IF EXISTS mat_mul(text, text, text);
-
-
-CREATE OR REPLACE FUNCTION elm_mul(A text, B text, i int, j int)
-RETURNS double precision
-LANGUAGE plpgsql
-AS $$
-DECLARE
-  res double precision;
-BEGIN
-  EXECUTE format(
-    'SELECT COALESCE(SUM(A.value * B.value), 0)
-     FROM %s AS A
-     JOIN %s AS B ON A.j = B.i
-     WHERE A.i = $1 AND B.j = $2',
-    A, B)
-    INTO res
-    USING i, j;
-
-  RETURN res;
-END;
-$$;
 
 
 
@@ -29,45 +7,23 @@ RETURNS void
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    n int;
-    Am int;
-    Bm int;
-    p int;
     i int;
     j int;
     val double precision;
 BEGIN
-
-    EXECUTE format('SELECT MAX(i), MAX(j) FROM %s', A)
-    INTO n, Am;
-    EXECUTE format('SELECT MAX(i), MAX(j) FROM %s', B)
-    INTO Bm, p;
-    IF Am != Bm THEN
-        RAISE EXCEPTION 'Incompatible dimensions';
-    END IF;
-
-
     EXECUTE format('DROP TABLE IF EXISTS %s', C);
     EXECUTE format('
-        CREATE TABLE %s (
-            i INT NOT NULL,
-            j INT NOT NULL,
-            value DOUBLE PRECISION NOT NULL,
-            PRIMARY KEY (i, j)
-        )', C);
-
-
-    FOR i IN 1..n LOOP
-        FOR j IN 1..p LOOP
-            val := elm_mul(A, B, i, j);
-            EXECUTE format('INSERT INTO %s (i, j, value) VALUES ($1, $2, $3)', C)
-            USING i, j, val;
-        END LOOP;
-    END LOOP;
+        CREATE TABLE %s AS
+        SELECT A.i, B.j, SUM(A.value * B.value) AS value
+        FROM %s as A JOIN %s as B ON A.j = B.i
+        GROUP BY A.i, B.j',
+    C, A, B);
+    EXECUTE format('ALTER TABLE %s ADD PRIMARY KEY (i, j)', C);
 END;
 $$;
 
 
-SELECT mat_mul('a','a','b');
+
 SELECT matrix FROM mat_vis('a');
+SELECT mat_mul('a','a','b');
 SELECT matrix FROM mat_vis('b');
